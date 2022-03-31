c71d44006c4e5ebf602f45c79f800e0815edf877
Robin Reuteler <reutelerr@hotmail.com>
2022-04-01 00:37:26 +0200

    Added CSV as readable file format, corrected filtering, refactoring

--- src/kmer_Counting.py
+++ src/kmer_Counting.py
@@ -1,11 +1,17 @@
+import json
+import os
 import sys
+import time
+
 import numpy as np
 from tqdm import tqdm
 import pickle
 
-minimumFrequencyThreshold = 1.0
+minimumFrequencyThreshold = 2.0
 seqLimiter = 100000
-maxLength = 5
+maxLength = 10
+
+
 
 #Gets the maximum frequency of this sequence as a subsequence of longer ones
 def occurenceFreqAsSubSeq(seq, frequent_seq_indexes):
@@ -14,64 +20,101 @@ def occurenceFreqAsSubSeq(seq, frequent_seq_indexes):
         for frequentSeq in seqMap:
 
             # Is seq a subsequence of frequentSeq, and does it occur more than the current max ?
-            if seq in frequentSeq and len(seqMap[frequentSeq]) > maxOccurenceFreq:
-                maxOccurenceFreq = len(seqMap[frequentSeq])
+            if seq in frequentSeq and seqMap[frequentSeq] > maxOccurenceFreq:
+                maxOccurenceFreq = seqMap[frequentSeq]
     return maxOccurenceFreq
 
-def countKmers(mode, indexFilePath, sourcePath):
-    for length in tqdm(range(maxLength, 0, -1)):
-        sequence_file = open("../"+sourcePath)
+def countKmers(sequence, sequenceCounts, nucleotideCounter, kmerLength) :
+    for i in range(len(sequence) - kmerLength + 1):
+        nucleotideCounter += 1
+        kmer = sequence[i:i + kmerLength]
+        if sequenceCounts.__contains__(kmer):
+            sequenceCounts[kmer] += 1
+        else:
+            sequenceCounts[kmer] = 1
+    return nucleotideCounter + kmerLength
 
-        sequenceCount = 0
-        sequence = ""
-        sequenceIndexes = {}
-        nucleotideCounter = 0
 
-        for line in sequence_file:
-            if sequenceCount < seqLimiter:
-                if line[0] == '>':
-                    if sequence != "":
-                        for i in range(len(sequence)-length+1):
-                            nucleotideCounter+=1
-                            kmer = sequence[i:i+length]
-                            if sequenceIndexes.__contains__(kmer):
-                                sequenceIndexes[kmer].append(nucleotideCounter)
-                            else:
-                                sequenceIndexes[kmer] = [nucleotideCounter]
-                        #print(len(sequence))
-                    #print(line)
-                    sequence = ""
-                    sequenceCount+=1
-                else:
-                    rawLine = line[:len(line)-1]
-                    sequence = sequence + rawLine#Stitching the sequence back together line by line
-                    # TODO find a way to deal with very long sequences
-        if sequence != "":
-            for i in range(len(sequence) - length + 1):
-                nucleotideCounter += 1
-                kmer = sequence[i:i + length]
-                if sequenceIndexes.__contains__(kmer):
-                    sequenceIndexes[kmer].append(nucleotideCounter)
-                else:
-                    sequenceIndexes[kmer] = [nucleotideCounter]
+def readSequenceFile(mode, indexFilePath, sourcePath, sourceType):
+    sequence_file = open("../"+sourcePath)
+    def blocks(file, size=65536):
+        while True:
+            b = file.read(size)
+            if not b: break
+            yield b
 
-        sequenceIndexesFilename = indexFilePath + "/sequences" +str(length)
-        sequenceIndexesFile = open(sequenceIndexesFilename, "wb") # TODO create folder dynamically aswell
-        pickle.dump((nucleotideCounter, sequenceIndexes), sequenceIndexesFile)
-        print(sequenceCount)
+    noLines = sum(bl.count("\n") for bl in blocks(sequence_file))
+    print("Total number of lines in file : "+str(noLines))
+
+    for length in range(maxLength, 2, -1):
+        print("Counting K-mers of length : "+str(length))
+        sequence_file.seek(0)
+        time.sleep(0.01)
+
+        with tqdm(total=noLines, position=0, leave=True) as pbar:
+
+            sequenceCount = 0
+            sequence = ""
+            sequenceIndexes = {}
+            nucleotideCounter = 0
+
+            #Reading the file
+            if sourceType == 0 :
+                for line in sequence_file:
+                    pbar.update(1)
+                    if sequenceCount < seqLimiter:
+                        if line[0] == '>':
+                            if sequence != "":
+                                nucleotideCounter = countKmers(sequence, sequenceIndexes, nucleotideCounter, length)
+                            sequence = ""
+                            sequenceCount+=1
+                        else:
+                            rawLine = line[:len(line)-1]
+                            sequence = sequence + rawLine#Stitching the sequence back together line by line
+                            # TODO find a way to deal with very long sequences
+                if sequence != "":
+                    countKmers(sequence, sequenceIndexes, nucleotideCounter, length)
+            if sourceType == 1:
+                headers = sequence_file.readline().split(',')
+                line = sequence_file.readline()
+                while line:
+                    pbar.update(1)
+                    lineElements = line.split(',')
+                    sequence = lineElements[-1].lower()
+                    nucleotideCounter = countKmers(sequence, sequenceIndexes, nucleotideCounter, length)
+                    line = sequence_file.readline()
+
+
+
+            sequenceIndexesFilename = indexFilePath + "/sequences" +str(length) + ".json"
+            os.makedirs(os.path.dirname(sequenceIndexesFilename), exist_ok=True)
+            sequenceIndexesFile = open(sequenceIndexesFilename, "w")
+            sequenceIndexesFile.write(json.dumps((nucleotideCounter, sequenceIndexes), indent=4))
+
+    sequence_file.close()
 
 
 def filterByFrequency(indexFilePath):
-    frequent_sequence_indexes = [{}]*maxLength
-    for length in range(maxLength, 0, -1):
-        (nucleotideCount, sequence_indexes) = pickle.load(open(indexFilePath+"/sequences"+str(length), "rb"))
+    frequent_sequence_indexes = [None]*maxLength
+    for length in range(maxLength, 2, -1):
+        sequenceCountFile = open(indexFilePath+"/sequences"+str(length)+".json", "rb")
+        (nucleotideCount, sequence_indexes) = json.load(sequenceCountFile)
+        frequent_sequence_indexes[length - 1] = {}
+
 
         for kmer in tqdm(sequence_indexes):
-            noOfOccurences = len(sequence_indexes[kmer])
-            if noOfOccurences/nucleotideCount > pow(0.25, len(kmer)) * minimumFrequencyThreshold:
-                noOfEffectiveOccurences = len(sequence_indexes[kmer]) - occurenceFreqAsSubSeq(kmer, frequent_sequence_indexes[length:maxLength - 1])
-                if noOfEffectiveOccurences / nucleotideCount > pow(0.25, len(kmer)) * minimumFrequencyThreshold:  # Removing any non-frequent kner
-                    frequent_sequence_indexes[length - 1][kmer] = sequence_indexes[kmer]
+            noOfOccurences = sequence_indexes[kmer]
+            if noOfOccurences > nucleotideCount/pow(4, len(kmer)) * minimumFrequencyThreshold:
+                # occurences independent of longer frequent seq
+                #noOfEffectiveOccurences = sequence_indexes[kmer] - occurenceFreqAsSubSeq(kmer, frequent_sequence_indexes[length:maxLength - 1])
+                #if noOfEffectiveOccurences > nucleotideCount/pow(4, len(kmer)) * minimumFrequencyThreshold :  # Removing any non-frequent kner
+                    dic = frequent_sequence_indexes[length - 1]
+                    dic[kmer] = sequence_indexes[kmer]
+
+        sequenceCountFile.close()
+        sequenceCountFile = open(indexFilePath + "/sequences" + str(length) + ".json", "w")
+        sequenceCountFile.write(json.dumps((nucleotideCount, frequent_sequence_indexes[length-1]), indent=4))
+        sequenceCountFile.close()
 
 
 
--- src/main.py
+++ src/main.py
@@ -6,6 +6,9 @@ import kmer_Counting
 NEW = 0
 ADD = 1
 
+FASTA = 0
+CSV = 1
+
 def displayHelp():
     print(
         "AVAILIBLE COMMANDS\n"
@@ -16,15 +19,19 @@ def displayHelp():
 
 def Index(mode, name, sourcePath):
     destinationPath = "../sequenceIndexFiles/"+name
+    fileExtension = sourcePath.split('.')[-1]
+    if fileExtension == "csv" : sourceType = CSV
+    if fileExtension == "fasta" : sourceType = FASTA
+    if fileExtension == "txt" : sourceType = FASTA
 
     start_time = time.time()
-    kmer_Counting.countKmers(mode, destinationPath, sourcePath)
+    kmer_Counting.readSequenceFile(mode, destinationPath, sourcePath, sourceType)
     print("--- %s seconds ---" % (time.time() - start_time))
 
 def Filter(name):
     indexFilePath = "../sequenceIndexFiles/"+name
     start_time = time.time()
-    kmer_Counting.filterByFrequency()
+    kmer_Counting.filterByFrequency(indexFilePath)
     print("--- %s seconds ---" % (time.time() - start_time))
 
 #print(f"Arguments count: {len(sys.argv)}")
