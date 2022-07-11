d65c2f7519d96ccd668606c6e6641a7fca9d78cf
robin reuteler <reutelerr@hotmail.com>
2022-07-11 18:36:09 +0200

    GridSeach on MLP

--- src/modelTrainingAndTesting.py
+++ src/modelTrainingAndTesting.py
@@ -4,7 +4,7 @@ import random
 
 import numpy
 import sklearn
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 
@@ -38,13 +38,13 @@ def prepareData(seqFilePath, labelsFilePath):
     seq_BOWs = [counts for (index, nulceotideCount, vecLength, counts) in data]
     countValues = numpy.array([list(seq_BOW.values()) for seq_BOW in seq_BOWs])
     labels = labelsFile.readlines()
-    return (countValues, labels)
+    return  countValues, labels
 
 @timerWrapper
 def trainModel(model, dataset, labels):
-    scores = cross_val_score(model, dataset, labels)
     model.fit(dataset, labels)
-    return (model, scores)
+    return model
+
 
 def trainAndTestKNN(modelType, trainingFilePath, trainingLabelsFilePath, testingFilePath, testingLabelsFilePath):
     random.seed()
@@ -55,50 +55,35 @@ def trainAndTestKNN(modelType, trainingFilePath, trainingLabelsFilePath, testing
     scaledTrainingBOWs = scaler.transform(trainingBOWs)
     scaledAndNormalizedTrainingBOWs = sklearn.preprocessing.normalize(scaledTrainingBOWs)
     model = None
-    modelScaled = None
-    modelScaledAndNormalized = None
-
+    gridSearcher = None
 
     print("Model type : "+modelType)
-    if(modelType=="KNN"):
+    if modelType== "KNN":
         n_neighbors = 5
         model = KNeighborsClassifier(n_neighbors=n_neighbors)
-        modelScaled = KNeighborsClassifier(n_neighbors=n_neighbors)
-        modelScaledAndNormalized = KNeighborsClassifier(n_neighbors=n_neighbors)
         print("n_neighbors : "+str(n_neighbors))
-    if(modelType=="Perceptron"):
-        hidden_layer_sizes = (10)
+    if modelType== "Perceptron":
+        hidden_layer_sizes = [(3), (15), (10), (15), (10, 3), (15, 3)]
+        solvers = ('sgd', 'lbfgs')
+        parameters = {'hidden_layer_sizes':hidden_layer_sizes, 'solver':('sgd', 'lbfgs')}
         random_state = random.randint(1, 100)
-        model = MLPClassifier(solver='sgd', hidden_layer_sizes=hidden_layer_sizes, random_state=random_state)
-        modelScaled = MLPClassifier(solver='sgd', hidden_layer_sizes=hidden_layer_sizes, random_state=random_state)
-        modelScaledAndNormalized = MLPClassifier(solver='sgd', hidden_layer_sizes=hidden_layer_sizes, random_state=random_state)
+        model = MLPClassifier()
+        gridSearcher = GridSearchCV(model, parameters, cv=5, scoring='accuracy', verbose=4)
         print("hidden_layer_sizes : "+str(hidden_layer_sizes))
+        print("Solvers : "+str(solvers))
         print("random_state : "+str(random_state))
 
-    print("Training model (unscaled)")
-    (model, scores) = trainModel(model, trainingBOWs, trainingLabels)
-
     print("Training model (scaled)")
-    (modelScaled, scoresScaled) = trainModel(modelScaled, scaledTrainingBOWs, trainingLabels)
-
-    print("Training model (scaled and normalized)")
-    (modelScaledAndNormalized, scoresScaledAndNormalized) = trainModel(modelScaledAndNormalized, scaledAndNormalizedTrainingBOWs, trainingLabels)
+    (gridSearcher) = trainModel(gridSearcher, scaledTrainingBOWs, trainingLabels)
 
     print("Reading testing data")
     (testingBOWs, testingLabels) = prepareData(testingFilePath, testingLabelsFilePath)
     scaledTestingBOWs = sklearn.preprocessing.scale(testingBOWs)
-    scaledAndNormalizedTestingBOWs = sklearn.preprocessing.normalize(scaledTestingBOWs)
 
     print("Testing model")
-    testing_score = model.score(testingBOWs, testingLabels)
-    testing_scaled_score = modelScaled.score(scaledTestingBOWs, testingLabels)
-    testing_scaled_normalized_score = modelScaledAndNormalized.score(scaledAndNormalizedTestingBOWs, testingLabels)
-    print("Unscaled scores :"+str(scores))
-    print('->'+str(testing_score))
-    print("Scaled scores :"+str(scoresScaled))
-    print('->'+str(testing_scaled_score))
-    print("Scaled and Normalized scores :"+str(scoresScaledAndNormalized))
-    print('->'+str(testing_scaled_normalized_score))
+    testing_scaled_score = gridSearcher.score(scaledTestingBOWs, testingLabels)
+    print('->' + str(testing_scaled_score))
+    print("Best params : "+str(gridSearcher.best_params_))
 
 
 
